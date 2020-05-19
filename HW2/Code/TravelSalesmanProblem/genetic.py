72afae3d89feabca8d17732acbfa0c87852d6d0a
muhammadreza <mra.akhgari@gmail.com>
2020-05-19 13:43:35 +0430

    create body of algorithm

--- HW2/Code/TravelSalesmanProblem/genetic.py
+++ HW2/Code/TravelSalesmanProblem/genetic.py
@@ -12,7 +12,29 @@ class genetic:
     # initial population in first time.
     def initial_population(self, population_size):
         population = []
-        for i in range(population_size):
+        for _ in range(population_size):
             population.append(self.create_path())
         return population
 
+    def next_generation(self, current_population, elite_size, mutation_rate):
+        population_raked = self.rank_path(current_population)
+        selection_results = self.selection(population_raked, elite_size)
+        mating_pool = self.mating_pool(current_population, selection_results)
+        children = self.breed_population(mating_pool, elite_size)
+        next_population = self.mutate_population(children, mutation_rate)
+        return next_population
+
+    def rank_path(self, current_population):
+        pass
+
+    def selection(self, population_raked, elite_size):
+        pass
+
+    def mating_pool(self, current_population, selection_results):
+        pass
+
+    def breed_population(self, mating_pool, elite_size):
+        pass
+
+    def mutate_population(self, children, mutation_rate):
+        return None
